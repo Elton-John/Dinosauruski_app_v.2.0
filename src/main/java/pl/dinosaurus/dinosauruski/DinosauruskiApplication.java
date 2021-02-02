19e1f4e33ac7663f947a2a58fca8de2654015f79
Olga <nauka.dinosaurus@gmail.com>
2021-02-02 09:50:27 +0100

    Initial commit

---
+++ src/main/java/pl/dinosaurus/dinosauruski/DinosauruskiApplication.java
@@ -0,0 +1,13 @@
+package pl.dinosaurus.dinosauruski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinosauruskiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinosauruskiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dinosaurus/dinosauruski/DinosauruskiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dinosaurus.dinosauruski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinosauruskiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
