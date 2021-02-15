96e73d6a59667237032f291fbe89ab35c3ca0db9
Olga <nauka.dinosaurus@gmail.com>
2021-02-15 19:18:53 +0100

    Refactoring code

---
+++ src/main/java/pl/dinosaurus/dinosauruski/teacherRole/CockpitController.java
@@ -0,0 +1,47 @@
+package pl.dinosaurus.dinosauruski.teacherRole;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.dinosaurus.dinosauruski.model.CurrentUser;
+import pl.dinosaurus.dinosauruski.model.Slot;
+import pl.dinosaurus.dinosauruski.model.Student;
+import pl.dinosaurus.dinosauruski.model.Teacher;
+import pl.dinosaurus.dinosauruski.student.StudentService;
+import pl.dinosaurus.dinosauruski.teacherRole.teacher.TeacherService;
+
+import java.util.List;
+
+@Controller
+@Secured("ROLE_TEACHER")
+public class CockpitController {
+
+    private final StudentService studentService;
+    private final TeacherService teacherService;
+
+    public CockpitController(StudentService studentService, TeacherService teacherService) {
+        this.studentService = studentService;
+        this.teacherService = teacherService;
+    }
+
+    @GetMapping("/teacher/cockpit")
+    public String cockpit(@AuthenticationPrincipal CurrentUser currentUser, Model model) {
+        Teacher teacher = (Teacher) currentUser.getUser();
+        model.addAttribute("teacher", teacher);
+        List<Slot> slots = teacherService.findAllFreeSlotsByTeacherId(teacher.getId());
+        model.addAttribute("freeSlots", slots);
+        return "teacher/cockpit/home";
+    }
+
+    @GetMapping("teacher/students")
+    public String studentsList(@AuthenticationPrincipal CurrentUser currentUser, Model model) {
+        Teacher teacher = (Teacher) currentUser.getUser();
+        model.addAttribute("teacher", teacher);
+        List<Student> students = studentService.findAllStudentsByTeacherId(teacher.getId());
+        model.addAttribute("students", students);
+        return "teacher/cockpit/students";
+    }
+
+}
